854aea724a3b490c03ab0e647c9ab67049540a20
Suhaib200119 <sis.22.18a@gmail.com>
2023-05-14 19:54:26 +0300

    add validation

--- models/user_expenses.js
+++ models/user_expenses.js
@@ -1,11 +1,24 @@
 
 const mongoose=require("mongoose");
 const userExpensesSchema=new mongoose.Schema({
-    userSnn:String,
+    userSnn:{
+        type:String,
+        required:true,
+        minlength:[9,"the min length 9"]
+     },
     expensesId:Number,
-    expensesName:String,
-    expensesDate:String,
-    expensesValue:Number,
+    expensesName:{
+        type:String,
+        required:true,
+     },
+    expensesDate:{
+        type:String,
+        required:true,
+     },
+    expensesValue:{
+        type:Number,
+        required:true,
+     },
     createdAt: { type: Date, default: Date.now(), expires: 60*60*24*30 },
 });
 
--- models/users.js
+++ models/users.js
@@ -1,11 +1,32 @@
 const mongoose=require("mongoose");
 const usersSchema=new mongoose.Schema({
-   username:String,
-   email:String,
-   password:String,
-   salary:Number,
-   reminderSalary:Number,
-   snn:String
+   username:{
+      type:String,
+      required:true,
+   },
+   email:{
+      type:String,
+      required:true,
+      uniqe:true,
+      match:[/.+\@.+\..+/,"invalid email!"],
+   },
+   password:{
+      type:String,
+      required:true,
+   },
+   salary:{
+      type:Number,
+      required:true,
+   },
+   reminderSalary:{
+      type:Number,
+      required:true,
+   },
+   snn:{
+      type:String,
+      required:true,
+      minlength:[9,"the min length 9"]
+   },
 });
 
 module.exports=mongoose.model("users",usersSchema);
